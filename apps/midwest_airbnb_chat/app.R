da4360f1e670404da04d06074426324f868cfdd7
carothlj-source <carothlj@miamioh.edu>
2026-09-23 18:51:02 -0400

    Updating app interface

--- apps/midwest_airbnb_chat/app.R
+++ apps/midwest_airbnb_chat/app.R
@@ -1,3 +1,5 @@
+library(shiny)
+library(bslib)
 
 con = DBI::dbConnect(RSQLite::SQLite(), "data/midwest_airbnb.db")
 
@@ -15,4 +17,31 @@ qc = querychat::querychat(
   data_description = "data/data_description.md"
 )
 
-qc$app_obj()
+ui = page_sidebar(
+  title = "Midwest Airbnb Chat",
+    theme = bs_theme(
+      primary = "#000080",
+      secondary = "#950606",
+      base_font = font_collection("Times New Roman")),
+    sidebar = qc$sidebar(width = 350),
+      card(card_header(textOutput("title")),
+           DT::DTOutput("table")),
+        accordion(open = FALSE,
+                accordion_panel("About", "Explore Airbnb listings from Chicago, Columbus, and the Twin Cities. Ask about prices, ratings, hosts, availability, amenities, and more!"))
+)
+
+server = function(input, output, session) {
+  vals = qc$server()
+  output$title = renderText(
+    vals$title() %||% "All Airbnb Listings" )
+  output$table = DT::renderDT(
+    vals$df(),
+    options = list(pageLength = 10))
+  output$sql = renderText(
+    vals$sql() %||% "SELECT * FROM listings"
+  )
+}
+
+shinyApp(ui, server)
+
+
